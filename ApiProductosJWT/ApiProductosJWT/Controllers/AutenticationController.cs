1e736e7049e8768c1a95aaf01ecb7d53c4bf2831
EthiemGuerrero <ethiemalex03@gmail.com>
2022-11-28 21:23:16 -0500

    Terminar

---
+++ ApiProductosJWT/ApiProductosJWT/Controllers/AutenticationController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using ApiProductosJWT.Models;
+using System.Data;
+using System.Data.SqlClient;
+using BC = BCrypt.Net.BCrypt;
+
+namespace ApiProductosJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutenticationController : ControllerBase
+    {
+        private readonly string cadenaSQL;
+        private readonly string secretKey;
+
+        public AutenticationController(IConfiguration config)
+        {
+            secretKey = config.GetSection("settings").GetSection("secreteKey").Value;
+            cadenaSQL = config.GetConnectionString("CadenaSQL");
+        }
+
+        [HttpPost]
+        [Route("auth")]
+        public IActionResult Post(User user)
+        {
+            try
+            {
+                using (var conection = new SqlConnection(cadenaSQL))
+                {
+                    string password = null;
+                    int Id = 0;
+                    conection.Open();
+                    var cmd = new SqlCommand("Auth_usr", conection);
+                    cmd.Parameters.AddWithValue("email", user.email);
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            password = reader["password"].ToString();
+                            Id = Convert.ToInt32(reader["Id"].ToString());
+                            reader.Close();
+                        }
+                        else
+                        {
+                            return StatusCode(StatusCodes.Status400BadRequest, new { Message = "El Usuario no existe" });
+                        }
+                    }
+                    conection.Close(); 
+                    
+                    if(BC.Verify(user.password, password))
+                    {
+                        var keyBytes = Encoding.ASCII.GetBytes(secretKey);
+                        var claims = new ClaimsIdentity();
+                        claims.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.email));
+                        var tokenDescriptor = new SecurityTokenDescriptor
+                        {
+                            Subject = claims,
+                            Expires = DateTime.UtcNow.AddMinutes(5),
+                            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256Signature)
+                        };
+                        var tokenHandler = new JwtSecurityTokenHandler();
+                        var tokenConfig = tokenHandler.CreateToken(tokenDescriptor);
+                        string tokencreado = tokenHandler.WriteToken(tokenConfig);
+                        return StatusCode(StatusCodes.Status200OK, new { token = tokencreado });
+                    }
+                    else
+                    {
+                        return StatusCode(StatusCodes.Status401Unauthorized, new { token = "" });
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Message = ex.Message, Error = ex });
+
+            }
+        }
+    }
+}
---
+++ Bd.sql
@@ -0,0 +1,176 @@
+create database DBFACTURA;
+
+use DBFACTURA;
+
+create table Users(
+	Id int identity(100,1) primary key,
+	Name varchar(50) not null,
+	Lastname varchar(50) not null,
+	email varchar(100) not null unique,
+	password varchar(max) not null
+);
+
+
+-- Procedimientos
+
+go
+create procedure Auth_usr
+	@email varchar(100)
+as
+begin
+	SELECT password, Id from Users where email = @email;
+end
+
+go
+create procedure Select_usr
+	@Id int
+as
+begin
+	SELECT Name, Lastname, email from Users where Id = @Id;
+end
+
+go
+create procedure Create_usr
+	@Name varchar(50),
+	@Lastname varchar(50),
+	@email varchar(100),
+	@password varchar(max)
+as
+begin transaction TX_Create
+	begin try
+		INSERT INTO Users(Name, Lastname, email, password) values (@Name, @Lastname, @email,@password);
+		commit TX_Create
+		SELECT 'Usuario Creado correctamente' as Respuesta, 0 as Error
+	end try
+	begin catch
+		rollback TX_Create
+		SELECT ERROR_MESSAGE() as Respuesta, 1 as Error
+	end catch
+
+go
+create procedure Delete_usr
+	@Id int
+as
+begin transaction TX_Delete
+	BEGIN TRY
+		DELETE  FROM Users where Id = @Id
+		commit TX_Delete
+		SELECT 'Usuario eliminado Correctamente' as Respuesta, 0 as Error
+	END TRY
+	BEGIN CATCH
+		rollback TX_Delete
+		SELECT ERROR_MESSAGE() as Respuesta, 1 as Error
+	END CATCH
+
+go
+create procedure Update_usr
+	@Id int,
+	@Name varchar(50),
+	@Lastname varchar(50),
+	@email varchar(100),
+	@password varchar(max)
+as
+begin transaction TX_Update
+	BEGIN TRY
+		UPDATE Users set Name = ISNULL(@Name, Name), Lastname =ISNULL( @Lastname, Lastname), email =ISNULL( @email, email), password = ISNULL(@password, password) where Id = @Id
+		commit TX_Update
+		SELECT 'Usuario actualizado correctamente' as Respuesta, 0 as Error
+	END TRY
+	BEGIN CATCH
+		rollback TX_Update
+		SELECT ERROR_MESSAGE() as Respuesta, 1 as Error
+	END CATCH
+	
+
+----------------------------------------------
+create table PRODUCTO(
+	IdProducto int primary key identity,
+	CodigoBarra varchar(50) unique,
+	Nombre varchar(50),
+	Marca varchar(50),
+	Categoria varchar(100),
+	Precio decimal(10,2)
+);
+
+INSERT INTO PRODUCTO(CodigoBarra,Nombre,Marca,Categoria,Precio) values
+('50910010','Monitor Aoc - Curvo Gaming ','AOC','Tecnologia','1200'),
+('50910011','IdeaPad 3i','LENOVO','Tecnologia','1700'),
+('50910012','SoyMomo Tablet Lite','SOYMOMO','Tecnologia','300'),
+('50910013','Lavadora 21 KG WLA-21','WINIA','ElectroHogar','1749'),
+('50910014','Congelador 145 Lt Blanco','ELECTROLUX','ElectroHogar','779'),
+('50910015','Cafetera TH-130','THOMAS','ElectroHogar','119'),
+('50910016','Reloj análogo Hombre 058','GUESS','Accesorios','699'),
+('50910017','Billetera de Cuero Mujer Sophie','REYES','Accesorios','270'),
+('50910018','Bufanda Rec Mango Mujer','MANGO','Accesorios','169.90'),
+('50910019','Sofá Continental 3 Cuerpos','MICA','Muebles','1299'),
+('50910020','Futón New Elina 3 Cuerpos','MICA','Muebles','1349'),
+('50910021','Mesa Comedor Volterra 6 Sillas','TUHOME','Muebles','624.12');
+
+
+select * from PRODUCTO
+
+go
+create procedure sp_lista_productos
+	as
+	begin
+	select
+	IdProducto,CodigoBarra,Nombre,
+	Marca,Categoria,Precio
+	from PRODUCTO
+end
+go
+
+
+go
+create procedure sp_guardar_producto(
+	@codigoBarra varchar(50),
+	@nombre varchar(50),
+	@marca varchar(50),
+	@categoria varchar(100),
+	@precio decimal(10,2)
+)as
+begin transaction tx_guardar_producto
+	begin try
+		insert into PRODUCTO(CodigoBarra,Nombre,Marca,Categoria,Precio)values(@codigoBarra,@nombre,@marca,@categoria,@precio)
+		commit transaction tx_guardar_producto
+	end try
+	begin catch
+		rollback transaction tx_guardar_producto
+	end catch
+
+go
+create proc sp_editar_producto(
+	@idProducto int,
+	@codigoBarra varchar(50) null,
+	@nombre varchar(50) null,
+	@marca varchar(50) null,
+	@categoria varchar(100) null,
+	@precio decimal(10,2) null
+)as
+begin transaction tx_editar_producto
+	begin try
+		update PRODUCTO set
+		CodigoBarra = isnull(@codigoBarra,CodigoBarra),
+		Nombre = isnull(@nombre,Nombre),
+		Marca = isnull(@marca,Marca),
+		Categoria = isnull(@categoria,Categoria),
+		Precio = ISNULL(@precio,Precio)
+		where IdProducto = @idProducto;
+		commit transaction tx_editar_producto
+	end try
+	begin catch
+		rollback transaction tx_editar_producto
+	end catch
+
+go
+create proc sp_eliminar_producto(
+	@idProducto int
+)as
+begin transaction tx_eliminar_producto
+	begin try
+		delete from PRODUCTO where IdProducto = @idProducto
+		commit transaction tx_eliminar_producto
+	end try
+	begin catch
+		rollback transaction tx_eliminar_producto
+	end catch
